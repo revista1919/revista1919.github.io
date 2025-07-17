1ce1e2479f7dd96d6a8daf6b603e6185d7b1503a
revista1919 <revistaestudiantespentauc@gmail.com>
2025-07-17 03:16:56 +0000

    Describe brevemente los cambios realizados

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../public/logo.png'; // Ruta corregida
+import logo from '../../public/logo.png';
 
 function Header() {
   return React.createElement(
@@ -10,16 +10,16 @@ function Header() {
     },
     React.createElement(
       'div',
-      { className: 'container flex items-center justify-between' },
+      { className: 'container flex flex-col sm:flex-row items-center justify-between' }, // Cambiado a flex-col en móviles
       React.createElement(
         'div',
-        { className: 'flex items-center' },
-        React.createElement('img', { src: logo, alt: 'Revista Logo', className: 'h-40 mr-5' }),
-        React.createElement('h1', { className: 'text-4xl font-semibold italic font-serif' }, 'Revista Nacional de las Ciencias para Estudiantes')
+        { className: 'flex items-center mb-4 sm:mb-0' }, // Margen inferior en móviles
+        React.createElement('img', { src: logo, alt: 'Revista Logo', className: 'h-24 sm:h-40 mr-0 sm:mr-5' }), // Imagen más pequeña en móviles
+        React.createElement('h1', { className: 'text-2xl sm:text-4xl font-semibold italic font-serif text-center sm:text-left' }, 'Revista Nacional de las Ciencias para Estudiantes') // Texto más pequeño y centrado en móviles
       ),
       React.createElement(
         'p',
-        { className: 'absolute bottom-2 right-4 text-cream-100 text-sm italic font-serif text-right' },
+        { className: 'absolute bottom-2 right-4 text-cream-100 text-xs sm:text-sm italic font-serif text-right' }, // Texto más pequeño en móviles
         'Una revista por y para estudiantes'
       )
     )
