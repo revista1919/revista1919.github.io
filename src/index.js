de3b9872ba76a96732f961ff8c8c8aa62dfe69be
revista1919 <revistaestudiantespentauc@gmail.com>
2026-02-22 13:39:40 -0300

    hola

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom'; // Cambiado a HashRouter
 import AppES from './App';
 import AppEN from './AppEN';
 import ReviewerResponsePage from './components/ReviewerResponsePage';
@@ -8,20 +8,13 @@ import ReviewerResponsePage from './components/ReviewerResponsePage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        {/* Ruta raíz - Español */}
         <Route path="/" element={<AppES />} />
-        
-        {/* Todas las rutas que empiecen con /en - Inglés */}
         <Route path="/en/*" element={<AppEN />} />
-        
-        {/* Ruta especial para revisores */}
         <Route path="/reviewer-response" element={<ReviewerResponsePage />} />
-        
-        {/* Cualquier otra ruta no encontrada - redirige a español */}
         <Route path="*" element={<AppES />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
