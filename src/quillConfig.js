af3e2d0f0b4d29ac07a1db1d245e9b8e590e7aff
revista1919 <revistaestudiantespentauc@gmail.com>
2026-08-28 01:02:31 +0000

    vale

--- src/quillConfig.js
+++ src/quillConfig.js
@@ -1,5 +1,10 @@
 import { Quill } from 'react-quill';
 import ImageResize from 'quill-image-resize-module-react';
+import QuillBetterTable from 'quill-better-table';
+import 'quill-better-table/dist/quill-better-table.css';
 
-// Register ImageResize module once, silently (won't overwrite if already registered)
+// Register modules once, silently (won't overwrite if already registered)
 Quill.register('modules/imageResize', ImageResize, true);
+Quill.register({
+  'modules/better-table': QuillBetterTable
+}, true);
